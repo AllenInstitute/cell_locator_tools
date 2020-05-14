a3ed1deadb517f619f4bff5636939bd2cdecc20d
danielsf <scott.daniel@alleninstitute.org>
2020-05-14 11:00:15 -0700

    add cmake_cmd.sh

---
+++ BICCNcode/cmake_cmd.sh
@@ -0,0 +1,7 @@
+cmake \
+-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.15 \
+-DCMAKE_CXX_COMPILER=g++ \
+-DCMAKE_CC_COMPILER=gcc \
+-DPYTHON_INCLUDE_DIR=/Users/scott.daniel/AllenInstitute/miniconda3/python3.7m/ \
+-DPYTHON_LIBRARY=/Users/scott.daniel/AllenInstitute/miniconda3/lib/ \
+../cell-locator
