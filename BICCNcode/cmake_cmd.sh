44bf787c157b44a77558048d15db35c4a2ad3266
danielsf <scott.daniel@alleninstitute.org>
2020-05-14 11:06:33 -0700

    try to specify PATH:

--- BICCNcode/cmake_cmd.sh
+++ BICCNcode/cmake_cmd.sh
@@ -1,7 +1,7 @@
 cmake \
 -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.15 \
--DCMAKE_CXX_COMPILER=g++ \
--DCMAKE_CC_COMPILER=gcc \
--DPYTHON_INCLUDE_DIR=/Users/scott.daniel/AllenInstitute/miniconda3/python3.7m/ \
--DPYTHON_LIBRARY=/Users/scott.daniel/AllenInstitute/miniconda3/lib/ \
+-DCMAKE_CXX_COMPILER:PATH=/usr/bin/g++ \
+-DCMAKE_CC_COMPILER:PATH=/usr/bin/gcc \
+-DPYTHON_INCLUDE_DIR:PATH=/Users/scott.daniel/AllenInstitute/miniconda3/python3.7m/ \
+-DPYTHON_LIBRARY:PATH=/Users/scott.daniel/AllenInstitute/miniconda3/lib/ \
 ../cell-locator
