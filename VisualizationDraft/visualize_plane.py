e4be290979b8ab9b9743576910be0fab0b96a270
danielsf <scott.daniel@alleninstitute.org>
2020-06-01 11:29:58 -0700

    add utility script to visualize an annotation

---
+++ VisualizationDraft/visualize_plane.py
@@ -0,0 +1,27 @@
+import matplotlib
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
+import SimpleITK
+import cell_locator_utils
+
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--annotation', type=str, default=None)
+    args = parser.parse_args()
+
+    if args.annotation is None:
+        raise RuntimeError("must specify annotation")
+
+    resolution = 25
+    img_name = 'atlasVolume.mhd'
+    img = SimpleITK.ReadImage(img_name)
+    img_data = SimpleITK.GetArrayFromImage(img)
+
+    brain_img = cell_locator_utils.BrainImage(img_data, resolution)
+    slice_img = brain_img.slice_img_from_annotation(args.annotation)
+
+    plt.figure(figsize=(15,15))
+    plt.imshow(slice_img)
+    plt.show()
