91b1d280568152918c483f41e0652071e2e51ed9
danielsf <scott.daniel@alleninstitute.org>
2020-06-01 17:28:32 -0700

    use unittest in test_vis.py

--- VisualizationDraft/test_vis.py
+++ VisualizationDraft/test_vis.py
@@ -12,55 +12,57 @@ import time
 import tempfile
 import hashlib
 
+import unittest
 import cell_locator_utils
 
-def do_analysis():
+class ImageGenerationTest(unittest.TestCase):
 
-    resolution = 25
-    img_name = 'atlasVolume.mhd'
-    img = SimpleITK.ReadImage(img_name)
-    img_data = SimpleITK.GetArrayFromImage(img)
-    brain_img = cell_locator_utils.BrainImage(img_data, resolution)
-    print(img_data.shape)
-    t0 = time.time()
+    def test_images(self):
+        resolution = 25
+        img_name = 'atlasVolume.mhd'
+        img = SimpleITK.ReadImage(img_name)
+        img_data = SimpleITK.GetArrayFromImage(img)
+        brain_img = cell_locator_utils.BrainImage(img_data, resolution)
+        print(img_data.shape)
+        t0 = time.time()
 
-    for dex in range(4):
-        t1 = time.time()
+        for dex in range(4):
+            t1 = time.time()
 
-        annotation_name = '../CellLocatorAnnotations/annotation_%d.json' % dex
-        img_name = tempfile.mkstemp(dir='.',
-                                prefix='annotation_%d_' % dex,
-                                suffix='.png')[1]
-        control_name = 'control_imgs/annotation_%d.png' % dex
-        assert os.path.isfile(control_name)
+            annotation_name = '../CellLocatorAnnotations/annotation_%d.json' % dex
+            img_name = tempfile.mkstemp(dir='.',
+                                    prefix='annotation_%d_' % dex,
+                                    suffix='.png')[1]
+            control_name = 'control_imgs/annotation_%d.png' % dex
+            assert os.path.isfile(control_name)
 
-        new_img = brain_img.slice_img_from_annotation(annotation_name)
-        plt.figure(figsize=(10,10))
-        plt.imshow(new_img)
-        plt.savefig(img_name)
-        print(img_name)
+            new_img = brain_img.slice_img_from_annotation(annotation_name)
+            plt.figure(figsize=(10,10))
+            plt.imshow(new_img)
+            plt.savefig(img_name)
+            print(img_name)
 
-        md5_control = hashlib.md5()
-        with open(control_name, 'rb') as in_file:
-            while True:
-                data = in_file.read(10000)
-                if len(data)==0:
-                    break
-                md5_control.update(data)
-        md5_test = hashlib.md5()
-        with open(img_name, 'rb') as in_file:
-            while True:
-                data = in_file.read(10000)
-                if len(data)==0:
-                    break
-                md5_test.update(data)
-        assert md5_test.hexdigest() == md5_control.hexdigest()
+            md5_control = hashlib.md5()
+            with open(control_name, 'rb') as in_file:
+                while True:
+                    data = in_file.read(10000)
+                    if len(data)==0:
+                        break
+                    md5_control.update(data)
+            md5_test = hashlib.md5()
+            with open(img_name, 'rb') as in_file:
+                while True:
+                    data = in_file.read(10000)
+                    if len(data)==0:
+                        break
+                    md5_test.update(data)
+            self.assertEqual(md5_test.hexdigest(), md5_control.hexdigest())
 
-        print(md5_test.hexdigest())
-        print(md5_control.hexdigest())
-        print('')
-        if os.path.exists(img_name):
-            os.unlink(img_name)
+            print(md5_test.hexdigest())
+            print(md5_control.hexdigest())
+            print('')
+            if os.path.exists(img_name):
+                os.unlink(img_name)
 
 if __name__ == "__main__":
-    do_analysis()
+    unittest.main()
