ab23ce7955d5fe14d3d3fa19cc7fd13f42a4af74
danielsf <scott.daniel@alleninstitute.org>
2020-06-02 13:56:39 -0700

    fix allen_to_pixel

--- VisualizationDraft/cell_locator_utils.py
+++ VisualizationDraft/cell_locator_utils.py
@@ -195,13 +195,8 @@ class BrainSlice(object):
 
         pixel_coords = np.NaN*np.ones((2,allen_coords.shape[1]), dtype=int)
         slice_coords = self.coord_converter.allen_to_slice(allen_coords[:,valid_dex])
-
-        xx = np.round(slice_coords[0,:]/self.resolution).astype(int)
-        pixel_coords[0,valid_dex] = xx-self.x_min_pix
-
-        yy = np.round(slice_coords[1,:]/self.resolution).astype(int)
-        pixel_coords[1,valid_dex] = yy-self.y_min_pix
-
+        valid_pixels = self.slice_to_pixel(slice_coords)
+        pixel_coords[:,valid_dex] = valid_pixels
         return pixel_coords, valid_dex
 
     def pixel_to_slice(self, pixel_coords):
