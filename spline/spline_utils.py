2d110527862319f51fa059b865614ff709363131
danielsf <scott.daniel@alleninstitute.org>
2020-06-04 14:39:13 -0700

    remove comment

--- spline/spline_utils.py
+++ spline/spline_utils.py
@@ -150,8 +150,6 @@ class Annotation(object):
                    bad_dex = np.where(dist>d_threshold)[0]
                    new_t = t[bad_dex]+0.5*(t[bad_dex+1]-t[bad_dex])
                    t = np.sort(np.concatenate([t, new_t]))
-            # shave the value at t=1 because it is equivalent
-            # to next segment's t=0
             border_x.append(xx)
             border_y.append(yy)
         border_x = np.concatenate(border_x)
