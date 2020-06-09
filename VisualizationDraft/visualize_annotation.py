1deb66a7a108eb152addc888b8c4ec2a2ce29e56
danielsf <scott.daniel@alleninstitute.org>
2020-06-09 15:07:07 -0700

    clean up visualize_annotation.py

--- VisualizationDraft/visualize_annotation.py
+++ VisualizationDraft/visualize_annotation.py
@@ -49,20 +49,7 @@ if __name__ == "__main__":
     print(np.abs(markup_slice_coords[2,:]).max())
     markup_slice_pixels = brain_slice.slice_to_pixel(markup_slice_coords[:2,:])
 
-    print('x ',markup_slice_coords[0,:])
-    print(brain_slice.x_min)
-    print('y ',markup_slice_coords[1,:])
-    print(brain_slice.y_min)
-    print('')
-
-    #annotation = spline_utils.Annotation(markup_slice_coords[0,:]-brain_slice.x_min,
-    #                                     markup_slice_coords[1,:]-brain_slice.y_min)
-
-
     annotation = spline_utils.Annotation(markup_slice_coords[0,:], markup_slice_coords[1,:])
-
-
-    corner_pt = brain_slice.slice_to_pixel(np.array([[brain_slice.x_min],[brain_slice.y_min]]))
 
     ann_mask = annotation.get_mask(brain_slice.resolution)
     mesh = np.meshgrid(np.arange(ann_mask.shape[0]), np.arange(ann_mask.shape[1]))
@@ -73,100 +60,26 @@ if __name__ == "__main__":
     ann_sp_pix = brain_slice.slice_to_pixel(np.array([annotation._spline.x,
                                                       annotation._spline.y]))
 
-    print('x')
-    print(ann_sp_pix[0,:])
-    print(markup_slice_pixels[0,:])
-    print('\ny')
-    print(ann_sp_pix[1,:])
-    print(markup_slice_pixels[1,:])
-
-    print('')
-
-    #t_mask = np.zeros(ann_mask.shape, dtype=bool)
-
-    #for ii in range(ann_mask.shape[0]):
-    #    t_mask[ann_mask.shape[0]-1-ii,:] = ann_mask[ii,:]
+    np.testing.assert_array_equal(ann_sp_pix, markup_slice_pixels)
 
     t_mask = ann_mask
 
-    plt.figure(figsize=(15,15))
-
-    #plt.subplot(2,2,1)
-
     val = slice_img.max()+2.0
-
-    print('val ',val)
-    print('mask sum ',ann_mask.sum())
-    print('img sum ',slice_img.sum())
-    print(ann_mask_pix.shape)
-    print(ann_mask_pix[0,:].min(),ann_mask_pix[0,:].max())
-    print(ann_mask_pix[1,:].min(),ann_mask_pix[1,:].max())
-    print(ann_mask.shape)
-    print(slice_img.shape)
 
     x0 = ann_mask_pix[0,:].min()
     x1 = ann_mask_pix[0,:].max()+1
     y0 = ann_mask_pix[1,:].min()
     y1 = ann_mask_pix[1,:].max()+1
 
-    #slice_img[y0:y1, x0:x1][t_mask] += val
-    #slice_img[y0:y1, x0:x1][t_mask] *= 0.5
-
     for ix, iy in zip(ann_mask_pix[0,:], ann_mask_pix[1,:]):
         if t_mask[iy-y0,ix-x0]:
             slice_img[iy,ix] += val
             slice_img[iy,ix]*=0.5
-
-    print('img sum intermediate ',slice_img.sum())
     for ix, iy in zip(ann_sp_pix[0,:], ann_sp_pix[1,:]):
         slice_img[iy,ix] = 3*val
 
-
-    print('img sum again ',slice_img.sum())
-    #slice_img[ann_mask_pix[1,:], ann_mask_pix[0,:]][ann_mask] *= 0.5
+    plt.figure(figsize=(15,15))
     plt.imshow(slice_img,zorder=1)
     plt.scatter(markup_slice_pixels[0,:], markup_slice_pixels[1,:],
                 color='r', zorder=2, s=1, marker='o', alpha=0.5)
-
-    plt.scatter(corner_pt[0,:], corner_pt[1,:], zorder=4, color='r', s=15)
-
-    print((markup_slice_coords[0,:]-brain_slice.x_min).min()/brain_slice.resolution)
-    print((markup_slice_coords[1,:]-brain_slice.y_min).min()/brain_slice.resolution)
-    print(markup_slice_pixels[1,:].min())
-
-    p=np.zeros((2,1),dtype=float)
-    #p[0,0] = annotation.x_min*brain_slice.resolution+brain_slice.x_min
-    #p[1,0] = annotation.y_min*brain_slice.resolution+brain_slice.y_min
-
-    p[0,0] = markup_slice_coords[0,:].min()
-    p[1,0] = markup_slice_coords[1,:].max()
-
-    px = brain_slice.slice_to_pixel(p)
-    print(px)
-    plt.savefig(args.outname)
-    exit()
-
-
-    plt.subplot(2,2,2)
-
-    sp_pix = annotation.wc_to_pixels(np.array([annotation._spline.x, annotation._spline.y]))
-
-    ann_mask = ann_mask.astype(int)
-    print('mask sum ',ann_mask.sum(),ann_mask.max())
-    for ix, iy in zip(sp_pix[0,:], sp_pix[1,:]):
-        ann_mask[iy,ix] = 2
-    print('mask sum ',ann_mask.sum(),ann_mask.max())
-    plt.imshow(ann_mask)
-
-    plt.subplot(2,2,4)
-    t_arr = np.arange(0,1.001,0.01)
-    bdry_x = []
-    bdry_y = []
-    for ii in range(len(annotation._spline.x)):
-        xx, yy = annotation._spline.values(ii,t_arr)
-        bdry_x.append(xx)
-        bdry_y.append(yy)
-    plt.plot(np.concatenate(bdry_x), np.concatenate(bdry_y), color='c', zorder=0)
-    plt.scatter(annotation._spline.x, annotation._spline.y, color='r', zorder=1)
-
     plt.savefig(args.outname)
