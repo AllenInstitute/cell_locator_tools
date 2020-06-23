4eea607d34faeda497f065ea1487c7c741913a92
danielsf <scott.daniel@alleninstitute.org>
2020-06-23 11:18:29 -0700

    fix import

--- VisualizationDraft/lean_voxel.py
+++ VisualizationDraft/lean_voxel.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(this_dir.replace('VisualizationDraft','geom_package'))
+
 import json
 import numpy as np
 
