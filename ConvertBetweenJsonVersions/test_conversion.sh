2ee61f8d05cc8a86ae13a1a6cf1aa65d991da636
Lydia Ng <lydian@alleninstitute.org>
2021-04-05 19:19:47 -0700

    scripts to convert old to new json files

--- ConvertBetweenJsonVersions/test_conversion.sh
+++ ConvertBetweenJsonVersions/test_conversion.sh
@@ -1,3 +1,7 @@
-rm test_out.nrrd
-python convert_new_to_old_json.py --json_name data/multiple_2020-09-18.json --out_name test_20210402_out.nrrd --atlas_name average_template_10.nrrd
-diff test_result/test_20210402.nrrd test_20210402_out.nrrd
+rm -r test_out
+python convert_new_to_old_json.py --json_name data/multiple_2020-09-18.json --out_name test_out
+diff -q test_result_new_to_old test_out
+
+rm  test_out.json
+python convert_old_to_new_json.py --json_name test_result_new_to_old --out_name test_out.json
+diff test_result_new_to_old/converted.json test_out.json
